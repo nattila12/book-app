66588ad4e5ed572fd2f22f5ff3a59b92bf3f1f78
Nagy Attyla <nagyattyla@Attilas-MBP.localdomain>
2019-02-02 14:28:17 +0200

    change display books

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,32 +1,4 @@
 
-
-function displayBooks(books){
-    console.log('displayBooks', books)
-    var rows = books.map(function(book) {
-        console.log('transform book', book);
-        return `<tr>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.book-nr}</td>
-        </tr>`;
-    });
-    console.warn('rows', rows);
-    
-    //rows.push(getNewRow()); // simplified
-    var actions = getNewRow();
-    rows.push(actions);
-
-    document.querySelector('tbody').innerHTML = rows.join('');
-}
-
-function getNewRow(){
-    return 
-}
-
-function initEvents(){
-    console.log('initEvents')
-}
-
 function loadBooks(){
     $.ajax('/books.json').done(function(books){
         console.log('books');
@@ -37,6 +9,27 @@ function loadBooks(){
     
     console.log('loadBooks')
 }
+
+
+function displayBooks(books){
+    console.log('displayBooks', books)
+    var rows = books.map(function(book) {
+        console.log('transform book', book);
+        return `<tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.number}</td>
+        </tr>`;
+    });
+}
+
+
+
+function initEvents(){
+    console.log('initEvents')
+}
+
+
 
 function search(){
     console.log('search')
