fdc38f4008983667cdad2628ed59349ac5758ac9
al3xza <zaharia.ale@gmail.com>
2019-02-02 13:10:03 +0200

    added js functions

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,21 @@
 console.log('ksndkna')
 
+function displayBooks(){
+    console.log('displayBooks')
+}
+
+function initEvents(){
+    console.log('initEvents')
+}
+
+function loadBooks(){
+    console.log('loadBooks')
+    displayBooks();
+}
+
+function search(){
+    console.log('search')
+}
+
+loadBooks();
+initEvents();
